e5c1938be15549db60986a173634a29eb01d5b36
Reyqal <akureyqal@gmail.com>
2025-05-02 09:31:52 +0800

    Pertemuan 8

---
+++ Pertemuan ke 8/script.js
@@ -0,0 +1,66 @@
+console.log("Hello World");
+
+// Data type
+
+// Variable declare
+// String
+let soyuz = "Haloo soyuz";
+console.log(typeof soyuz);
+
+// Number
+const num = 2.998e9;
+console.log(num);
+console.log(typeof num);
+
+// Boolean
+const salah = false ;
+console.log(salah)
+console.log(typeof salah)
+
+// Conditional
+
+// // if
+// let name = "Agus";
+// if(name == "Reyqal"){
+//     console.log("Selamat datang " + name);
+// } 
+//     console.log("Nononono kamu bukan yang aku mau");
+
+
+// // if-else
+// let nama = "Reyqal";
+// if(nama == "Reyqal"){
+//     console.log("Selamat datang " + nama);
+// } else {
+//     console.log("Nononono kamu bukan yang aku mau")
+// }
+
+// if-else if-else
+let nama = "jayadi";
+
+if(nama == "Reyqal"){
+    console.log("Selamat datang " + nama);
+} else if (nama == "jayadi"){
+    console.log("Selamat datang " + nama);
+} else{
+    console.log("Nononono kamu bukan yang aku mau");
+}
+
+// Tru
+
+console.log(2 === "2");
+// console.log(2 == "B");
+// console.log("apel" > "apaa");
+// console.log("A" > "a");
+
+
+// - operator logika 
+
+// OR ||
+// AND &&
+// NOT !.
+
+
+
+
+
